9f3c3360b65f289e26cb4e401d443f3e9dd81069
Krishan Roy <krishanroy1989@gmail.com>
2024-01-19 12:34:48 +0600

    Implement BlocConsumer with AwesomeSnackBarContent

--- lib/main.dart
+++ lib/main.dart
@@ -48,19 +48,29 @@ class MyHomePage extends StatelessWidget {
               'You have pushed the button this many times:',
             ),
             const SizedBox(height: 30),
-            // BlocBuilder<CounterBloc, CounterState>(
-            //   // buildWhen: (previous, current) {
-            //   //   return current.counter >= 0;
-            //   // },
-            //   builder: (context, state) {
-            //     return Text(
-            //       '${state.counter}',
-            //       style: Theme.of(context).textTheme.headlineMedium,
-            //     );
-            //   },
-            // ),
+            /**
+             * BlocBuilder rebuilds/redraws the UI
+             */
+            /*
+             BlocBuilder<CounterBloc, CounterState>(
+              // buildWhen: (previous, current) {
+              //   return current.counter >= 0;
+              // },
+              builder: (context, state) {
+                return Text(
+                  '${state.counter}',
+                  style: Theme.of(context).textTheme.headlineMedium,
+                );
+              },
+            ),
+             */
 
-            BlocListener<CounterBloc, CounterState>(listener: (context, state) {
+            /**
+             * BlocListener does not rebuild the UI, it does something like showing dialog/snackbar/navigation etc.
+             */
+            /*
+            BlocListener<CounterBloc, CounterState>(
+              listener: (context, state) {
                 if (state.counter >= 5) {
                   final snackBar = SnackBar(
                     /// need to set following properties for best effect of awesome_snackbar_content
@@ -69,8 +79,7 @@ class MyHomePage extends StatelessWidget {
                     backgroundColor: Colors.transparent,
                     content: AwesomeSnackbarContent(
                       title: 'Working!',
-                      message:
-                      'snackbar is displayed!',
+                      message: 'snackbar is displayed!',
 
                       /// change contentType to ContentType.success, ContentType.warning or ContentType.help for variants
                       contentType: ContentType.success,
@@ -83,6 +92,41 @@ class MyHomePage extends StatelessWidget {
                 }
               },
               child: const Text('Bloc Listener'),
+            ),
+             */
+
+            /**
+             * Bloc Consumer Redraws as well as performs some action
+             */
+
+            BlocConsumer<CounterBloc, CounterState>(
+              builder: (context, state) {
+                return Text(
+                  '${state.counter}',
+                  style: const TextStyle(fontSize: 80),
+                );
+              },
+              listener: (context, state) {
+                if (state.counter >= 5) {
+                  final snackBar = SnackBar(
+                    /// need to set following properties for best effect of awesome_snackbar_content
+                    elevation: 0,
+                    behavior: SnackBarBehavior.floating,
+                    backgroundColor: Colors.transparent,
+                    content: AwesomeSnackbarContent(
+                      title: 'Working!',
+                      message: 'snackbar is displayed when >= 5!',
+
+                      /// change contentType to ContentType.success, ContentType.warning or ContentType.help for variants
+                      contentType: ContentType.success,
+                    ),
+                  );
+
+                  ScaffoldMessenger.of(context)
+                    ..hideCurrentSnackBar()
+                    ..showSnackBar(snackBar);
+                }
+              },
             ),
             const SizedBox(height: 50),
             Row(
