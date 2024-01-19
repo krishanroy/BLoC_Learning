ffce97b5f104f28c988313c66b1acb1bc24fc75d
Krishan Roy <krishanroy1989@gmail.com>
2024-01-19 10:44:40 +0600

    Implement the basic BLoC pattern with the counterApp from RipplesCode

---
+++ lib/counter_bloc/counter_state.dart
@@ -0,0 +1,17 @@
+abstract class CounterState {
+  int counter = 0;
+
+  CounterState({required this.counter});
+}
+
+class CounterInitialState extends CounterState {
+  CounterInitialState() : super(counter: 0);
+}
+
+class CounterIncrementState extends CounterState {
+  CounterIncrementState(int increasedCounter) : super(counter: increasedCounter);
+}
+
+class CounterDecrementState extends CounterState {
+  CounterDecrementState(int decreasedCounter) : super(counter: decreasedCounter);
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
+import 'package:bloc_learning/counter_bloc/counter_bloc.dart';
+import 'package:bloc_learning/counter_bloc/counter_event.dart';
+import 'package:bloc_learning/counter_bloc/counter_state.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,47 +13,31 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Counter BLoC',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+    return BlocProvider<CounterBloc>(
+      create: (context) => CounterBloc(),
+      child: MaterialApp(
+        title: 'Counter BLoC',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        home: const MyHomePage(title: 'Counter BLoC Home Page'),
       ),
-      home: const MyHomePage(title: 'Counter BLoC Home Page'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
+class MyHomePage extends StatelessWidget {
   const MyHomePage({super.key, required this.title});
 
   final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  void _decrementCounter() {
-    setState(() {
-      _counter--;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
+        title: Text(title),
       ),
       body: Center(
         child: Column(
@@ -59,22 +47,30 @@ class _MyHomePageState extends State<MyHomePage> {
               'You have pushed the button this many times:',
             ),
             const SizedBox(height: 30),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            BlocBuilder<CounterBloc, CounterState>(
+              builder: (context, state) {
+                return Text(
+                  '${state.counter}',
+                  style: Theme.of(context).textTheme.headlineMedium,
+                );
+              },
             ),
             const SizedBox(height: 50),
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
                 FloatingActionButton(
-                  onPressed: _incrementCounter,
-                  tooltip: 'Increment',
+                  onPressed: () {
+                    context.read<CounterBloc>().add(CounterDecrementEvent());
+                  },
+                  tooltip: 'Decrement',
                   child: const Icon(Icons.remove),
                 ),
                 FloatingActionButton(
-                  onPressed: _decrementCounter,
-                  tooltip: 'Decrement',
+                  onPressed: () {
+                    context.read<CounterBloc>().add(CounterIncrementEvent());
+                  },
+                  tooltip: 'Increment',
                   child: const Icon(Icons.add),
                 ),
               ],
